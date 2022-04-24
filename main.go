83aed0c4e8f983dbece5b0171f5d581c22670249
shanart <shanin.x@gmail.com>
2022-04-24 22:21:39 +0300

    duplicate renamer

--- main.go
+++ main.go
@@ -1,44 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"io/fs"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
 	"unicode"
 )
 
-func randomString(n int) string {
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-	s := make([]rune, n)
-	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(s)
-}
+func get_letter(n int) string {
+	result := []rune{}
+	if n > len(letterBytes) {
+		index := n / len(letterBytes)
+		ex := n - len(letterBytes)*index
+		for i := 0; i < index; i++ {
+			result = append(result, []rune(letterBytes)[i])
+		}
 
-func is_path_exists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
+		if ex > 0 {
+			result = append(result, []rune(letterBytes)[ex])
+		}
+		return string(result)
 	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
+	return string([]rune(letterBytes)[n])
 }
 
 func get_file_ext(file_name string) string {
 	return filepath.Ext(file_name)
-}
-
-func sanitize_name(name string, ext *string) string {
-	return strings.TrimSpace(strings.Replace(name, *ext, "", 1))
 }
 
 // Clean filename of all special characters and numbers
@@ -46,7 +38,7 @@ func sanitize_name(name string, ext *string) string {
 // And return cleaner file_name
 func name_cleaner(file fs.FileInfo) string {
 	file_ext := get_file_ext(file.Name())
-	file_name := sanitize_name(file.Name(), &file_ext)
+	file_name := strings.TrimSpace(strings.Replace(file.Name(), file_ext, "", 1))
 	var index int
 
 	for i, l := range file_name {
@@ -57,6 +49,19 @@ func name_cleaner(file fs.FileInfo) string {
 	}
 
 	return file_name[index:] + file_ext
+}
+
+func recreateFileName(path string, index int) string {
+	// Get new name
+	file_ext := get_file_ext(filepath.Base(path))
+	file_name := strings.TrimSpace(strings.Replace(filepath.Base(path), file_ext, "", 1))
+
+	if index > 0 {
+		file_name = file_name[:len(file_name)-2]
+	}
+	file_name = file_name + " " + get_letter(index) + file_ext
+	abs, _ := filepath.Abs(filepath.Join(strings.Replace(path, filepath.Base(path), "", 1), file_name))
+	return abs
 }
 
 func main() {
@@ -76,27 +81,30 @@ func main() {
 
 		for _, file := range files {
 			if !file.IsDir() {
+				i := 0
 				newFilePath := name_cleaner(file)
 				if newFilePath == file.Name() {
 					continue
 				}
 
 				abs, err := filepath.Abs(filepath.Join(path, newFilePath))
+				original_abs, _ := filepath.Abs(filepath.Join(path, file.Name()))
+				for {
+					if err != nil {
+						log.Fatal(err)
+					}
+					_, err = os.Stat(abs)
+					if os.IsNotExist(err) {
+						break
+					} else {
+						abs = recreateFileName(abs, i)
+					}
+					i++
+				}
+				err = os.Rename(original_abs, abs)
 				if err != nil {
 					log.Fatal(err)
 				}
-
-				_, err = os.Stat(abs)
-				if os.IsNotExist(err) {
-					fmt.Println("Process: ", abs)
-				} else {
-					fmt.Println("Rename: ", abs)
-				}
-
-				// err = os.Rename(abs, newFilePath)
-				// if err != nil {
-				// 	log.Fatal(err)
-				// }
 			}
 		}
 	} else {
